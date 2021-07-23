ff3827591d77b927e5a77bcd62837b9d292c5c3a
Jan Gorkiewicz <greencashew@users.noreply.github.com>
2021-07-23 23:02:09 +0200

    Initial commit

---
+++ src/main/java/com/greencashew/reactive/example/GithubUserGathererReactiveExampleApplication.java
@@ -0,0 +1,13 @@
+package com.greencashew.reactive.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubUserGathererReactiveExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubUserGathererReactiveExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/greencashew/reactive/example/GithubUserGathererReactiveExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greencashew.reactive.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubUserGathererReactiveExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
